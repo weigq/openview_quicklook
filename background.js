5f9147000c424da2370871257c5167e298d5fbe8
weigq <weigq1234@gmail.com>
2022-06-16 19:09:15 +0800

    update

--- background.js
+++ background.js
@@ -1,7 +1,7 @@
-chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
-    if (changeInfo.status === 'complete') {
-        chrome.tabs.sendMessage(tabId, {
-            message: 'TabUpdated'
-        });
-    }
-})
+// chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+//     if (changeInfo.status === 'complete') {
+//         chrome.tabs.sendMessage(tabId, {
+//             message: 'TabUpdated'
+//         });
+//     }
+// })
